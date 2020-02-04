2adb4faf3ea6923d27072c276e8a30ec47f20424
dagariya2 <dagariya2@gmail.com>
2020-02-04 14:27:25 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         <main className="container">
-          <Main />
-        </main>
+        <Main />        
       </div>
     );
   }
